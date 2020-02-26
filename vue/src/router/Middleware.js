45786b3de5379bf73e3b85d7eea071db0fd00ba6
Chris Taylor <christopherjamestaylor@outlook.com>
2020-02-26 12:13:12 +0000

    I have updated middleware

--- vue/src/router/Middleware.js
+++ vue/src/router/Middleware.js
@@ -1,13 +1,14 @@
-// import store from "@/state/store"
-//
-// export function checkAccessMiddleware(to, from, next) {
-//     if(store == ""){
-//         this.store.dispatch('/getSession').then((response) => {
-//             if(response){
-//                 next();
-//             }
-//         })
-//     } else{
-//         next();
-//     }
-// }
+import store from "@/state/store"
+
+export function checkAccessMiddleware(to, from, next) {
+    if(sessionStorage.getItem('user') !== null){
+        let data = {'username': sessionStorage.getItem('user'),};
+        store.dispatch('user/getUser', data).then((response) => {
+            if(response){
+                next();
+            }
+        })
+    } else{
+        next();
+    }
+}
