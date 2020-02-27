639dd63d2b915bb2bf97b5d0a993dc42cdbb3a34
Chris Taylor <christopherjamestaylor@outlook.com>
2020-02-27 09:12:15 +0000

    nutrition api found

---
+++ vue/src/api/constants.js
@@ -0,0 +1,22 @@
+const env = process.env;
+
+const getDomainEnv = (function(){
+    if (!env.FF_ENV) {
+        env.FF_ENV = env.NODE_ENV;
+    }
+    switch (env.FF_ENV) {
+        case 'local':
+            return 'http://localhost:5000';
+        case 'dev':
+        case 'stg':
+            return `//${env.FF_ENV}.freefitness.dev/api`;
+        default:
+        case 'live':
+        case 'production':
+            return '//freefitness.dev/api';
+    }
+});
+
+module.exports = {
+    api_url: getDomainEnv(),
+};
